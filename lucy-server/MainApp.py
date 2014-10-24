756e0a1401255080dfffc61c0a787582d1810e65
21zhou <21zhouyun@gmail.com>
2014-10-24 09:33:53 -0500

    add mysql databse support and write the received json to the databse

--- lucy-server/MainApp.py
+++ lucy-server/MainApp.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flaskext.mysql import MySQL
+import json
 
 
 mysql = MySQL()
@@ -12,7 +13,7 @@ mysql.init_app(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 #simple authenticate code to test if the database is connected with the MainApp
 #!!!IMPORTANT!!!
@@ -33,7 +34,35 @@ def authenticate():
 def receiver():
 	raw_json_str = request.args.get("json", "")
 	uuid = request.args.get("uuid", "")
+	data_type = request.args.get("data_type", "")
+	cursor = mysql.connect().cursor()
+	cursor.execute("SHOW TABLES LIKE %s" % data_type)
+	table = cursor.fetchone()
+
+	#if there is no table, don't do anything to the database
+	#rather than create a corresponding table for security reason
+	if table is None:
+		return "possible wrong data type"
+	else:
+		json_obj = parseJson(raw_json_str)
+		#for each row, we shoud have: uuid, data1, data2...
+		#since we don't know how many columns each data type may need, use another
+		#helper method to convert a json_object to list of rows
+		#and insert them
+		try:
+			cursor.execute("INSERT INTO %s VALUES (%s)" % (table, columns))
+			mysql.commit()
+		except:
+			mysql.rollback()
+
 	return "uuid: %s, json: %s" % (uuid, raw_json_str)
+
+def parseJson(json_str):
+	json_obj = json.load(json_str)
+	return json_obj
+
+def jsonObjToRowList(json_obj):
+
 
 if __name__ == "__main__":
 	app.debug = True
