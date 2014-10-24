b5907e09d0e2af46b82d1046465da44e9c632108
21zhou <21zhouyun@gmail.com>
2014-10-24 16:39:08 -0500

    tested parseJson

--- lucy-server/MainApp.py
+++ lucy-server/MainApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, g
 from flaskext.mysql import MySQL
 import json
 
@@ -11,6 +11,11 @@ app.config['MYSQL_DATABASE_DB'] = 'EmpData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+@app.before_request
+def before_request():
+	g.db = mysql.get_db()
+	g.cursor = mysql.get_db().cursor()
+
 @app.route("/")
 def hello():
 	return "Hello World!"
@@ -22,8 +27,7 @@ def hello():
 def authenticate():
 	username = request.args.get('UserName')
 	password = request.args.get('Password')
-	cursor = mysql.connect().cursor()
-	cursor.execute("SELECT * from User where userName = \'%s\' and password = \'%s\'" % (username, password))
+	g.cursor.execute("SELECT * from User where userName = \'%s\' and password = \'%s\'" % (username, password))
 	data = cursor.fetchone()
 	if data is None:
 		return "Username or Password is wrong"
@@ -33,12 +37,12 @@ def authenticate():
 @app.route("/upload", methods=["GET"])
 def receiver():
 	raw_json_str = request.args.get("json", "")
+	#return raw_json_str
 	uuid = request.args.get("uuid", "")
 	data_type = request.args.get("data_type", "")
-	cursor = mysql.connect().cursor()
-	cursor.execute("SHOW TABLES LIKE %s" % data_type)
-	table = cursor.fetchone()
-
+	g.cursor.execute("SHOW TABLES LIKE \'%s\'" % data_type)
+	#table = cursor.fetchone()
+	table = "a table"
 	#if there is no table, don't do anything to the database
 	#rather than create a corresponding table for security reason
 	if table is None:
@@ -49,20 +53,30 @@ def receiver():
 		#since we don't know how many columns each data type may need, use another
 		#helper method to convert a json_object to list of rows
 		#and insert them
-		try:
-			cursor.execute("INSERT INTO %s VALUES (%s)" % (table, columns))
-			mysql.commit()
-		except:
-			mysql.rollback()
-
-	return "uuid: %s, json: %s" % (uuid, raw_json_str)
-
-def parseJson(json_str):
-	json_obj = json.load(json_str)
+		#
+		
+		#try:
+		#	cursor.execute("INSERT INTO %s VALUES (%s)" % (table, columns))
+		#	g.db.commit()
+		#except:
+		#	g.db.rollback()
 	return json_obj
 
-def jsonObjToRowList(json_obj):
+def parseJson(json_str):
+	json_obj = json.loads(json_str)
+	return str(json_obj)
 
+#def jsonObjToRowList(json_obj):
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 if __name__ == "__main__":
 	app.debug = True
