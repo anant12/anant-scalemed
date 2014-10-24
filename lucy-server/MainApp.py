70196a51f1269c8ff5c33e774fb9dd95d83bf781
21zhou <21zhouyun@gmail.com>
2014-10-23 22:27:37 -0500

    tested http request can hold all the data.

--- lucy-server/MainApp.py
+++ lucy-server/MainApp.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/upload", methods=["GET"])
+def receiver():
+	raw_json_str = request.args.get("json", "")
+	uuid = request.args.get("uuid", "")
+	return "(uuid: %s, json: %s)" % (uuid, raw_json_str)
 
 if __name__ == "__main__":
     app.run()
