08ec9345605e4d44055665f2923e7ac96692ae09
21zhou <21zhouyun@gmail.com>
2014-10-25 16:31:02 -0500

    comment out test code, change databse user information'

--- lucy-server/MainApp.py
+++ lucy-server/MainApp.py
@@ -5,8 +5,8 @@ import json
 
 mysql = MySQL()
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '56289086'
+app.config['MYSQL_DATABASE_USER'] = 'loggingresearch'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'loggingresearchproject'
 app.config['MYSQL_DATABASE_DB'] = 'EmpData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -23,16 +23,16 @@ def hello():
 #simple authenticate code to test if the database is connected with the MainApp
 #!!!IMPORTANT!!!
 #this is no real authentication, it is just a test code!!
-@app.route("/Authenticate")
-def authenticate():
-	username = request.args.get('UserName')
-	password = request.args.get('Password')
-	g.cursor.execute("SELECT * from User where userName = \'%s\' and password = \'%s\'" % (username, password))
-	data = g.cursor.fetchone()
-	if data is None:
-		return "Username or Password is wrong"
-	else:
-		return "Logged in successfully"
+#@app.route("/Authenticate")
+#def authenticate():
+#	username = request.args.get('UserName')
+#	password = request.args.get('Password')
+#	g.cursor.execute("SELECT * from User where userName = \'%s\' and password = \'%s\'" % (username, password))
+#	data = g.cursor.fetchone()
+#	if data is None:
+#		return "Username or Password is wrong"
+#	else:
+#		return "Logged in successfully"
 
 @app.route("/upload", methods=["GET"])
 def receiver():
